dccef832634bfd8550428d137decf3fc15bff4ec
anup-paul <anuppaul1845@gmail.com>
2021-04-06 22:03:15 +0600

    update-project

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('dotenv').config()
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4mhth.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -43,9 +42,11 @@ client.connect(err => {
 
     app.get('/products', (req, res)=>
     {
+
         productsCollection.find({})
         .toArray((err, documents)=>
         {
+            console.log(err, documents)
             res.send(documents);
         })
     })
